cd6bd4527f39ef29977be40a96430425602a9461
klambycom <christian@klamby.com>
2018-06-06 19:33:44 +0200

    Update the Home route with filter and footer

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import image from './image.png';
 import './index.css';
+
+import Filter from "./components/Filter";
+import Page from "../../components/Page";
 
 class Home extends Component {
   render() {
     return (
-      <div className="Home">
-        <header className="Home-header">
-          <img src={logo} className="Home-logo" alt="logo" />
-          <h1 className="Home-title">Welcome to React</h1>
-        </header>
-        <p className="Home-intro">
-          To get started, edit <code>src/scenes/Home/index.js</code> and save to reload.
-        </p>
-      </div>
+      <Page className="Home">
+        <div className="Home--container">
+            <div className="Home--filter">
+              <header className="Home--header">
+                <h1>Hitta lediga lägenheter i Malmö</h1>
+              </header>
+              <Filter />
+            </div>
+            <div className="Home--image">
+              <img src={image} alt="Karta och Turning torso" />
+            </div>
+        </div>
+      </Page>
     );
   }
 }
